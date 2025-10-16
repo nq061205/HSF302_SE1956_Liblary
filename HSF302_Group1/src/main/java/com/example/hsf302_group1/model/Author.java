e991eac0fa08b22431b6fe6b3a3e06d7028eb3b4
DuyHM <hgduy03@gmail.com>
2025-10-16 16:59:17 +0700

    search author/package structure

--- HSF302_Group1/src/main/java/com/example/hsf302_group1/model/Author.java
+++ HSF302_Group1/src/main/java/com/example/hsf302_group1/model/Author.java
@@ -1,30 +1,37 @@
 package com.example.hsf302_group1.model;
 
 import jakarta.persistence.*;
-
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="authors")
+@Table(name = "authors")
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name="name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name="dob")
+    @Column(name = "dob")
     private String dob;
 
-    @Column(name="quotes")
+    @Column(name = "quotes", length = 500)
     private String quotes;
 
-    @ManyToMany @JoinTable(name="book_author",joinColumns = @JoinColumn(name="author_id"),inverseJoinColumns = @JoinColumn(name="book_id"))
-    private List<Book> bookList;
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Book> bookList = new ArrayList<>();
 
+    // ---------------- Constructors ----------------
+    public Author() {
+    }
 
-    public Author() {}
     public Author(int id, String name, String dob, String quotes) {
         this.id = id;
         this.name = name;
@@ -32,6 +39,7 @@ public class Author {
         this.quotes = quotes;
     }
 
+    // ---------------- Getters & Setters ----------------
     public int getId() {
         return id;
     }
@@ -64,6 +72,34 @@ public class Author {
         this.quotes = quotes;
     }
 
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    // ---------------- Helper Methods ----------------
+    /** Thêm một cuốn sách vào danh sách của tác giả */
+    public void addBook(Book book) {
+        if (!this.bookList.contains(book)) {
+            this.bookList.add(book);
+            // Nếu quan hệ 2 chiều, cần thêm dòng này:
+            // book.getAuthorList().add(this);
+        }
+    }
+
+    /** Xóa một cuốn sách khỏi danh sách của tác giả */
+    public void removeBook(Book book) {
+        if (this.bookList.contains(book)) {
+            this.bookList.remove(book);
+            // Nếu quan hệ 2 chiều, cần thêm dòng này:
+            // book.getAuthorList().remove(this);
+        }
+    }
+
+    // ---------------- ToString ----------------
     @Override
     public String toString() {
         return "Author{" +
